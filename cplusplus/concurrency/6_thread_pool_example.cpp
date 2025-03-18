6b98069032515751e847955b2db7b260a5b2df9c
xiaohan.xu@sectrend.com.cn <xiaohan.xu@sectrend.com.cn>
2025-03-18 14:30:32 +0800

    Add print msg for thread pool demo

--- cplusplus/concurrency/6_thread_pool_example.cpp
+++ cplusplus/concurrency/6_thread_pool_example.cpp
@@ -6,16 +6,15 @@
 #include "thread-pool.h"
 
 int main() {
+  fmt::print("begin\n");
 
   ThreadPool pool(4);
   std::vector<std::future<int>> results;
 
-  fmt::print("begine\n");
-
   for (int i = 0; i < 8; ++i) {
-    fmt::print("for loop {}", i);
+    fmt::print("iterator {}\n", i);
     results.emplace_back(pool.enqueue([i] {
-      fmt::print("hello {}\n", i);
+      fmt::print("enqueue {}\n", i);
       std::this_thread::sleep_for(std::chrono::seconds(1));
       return i * i;
     }));
